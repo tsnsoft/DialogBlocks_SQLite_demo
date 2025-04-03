bd135ec954df521f34fdf4a32767dfb64ae1c9c9
Talipov S.N. <proffix4@gmail.com>
2025-04-03 16:20:58 +0500

    rev. 20250403162058

--- CLEANING.bat
+++ CLEANING.bat
@@ -1,7 +1,6 @@
 @echo off
 setlocal enabledelayedexpansion
 
-REM Удаление файлов с определёнными расширениями
 for %%f in (*.bak) do del /Q /F "%%f" >nul 2>&1
 for %%f in (*.sln) do del /Q /F "%%f" >nul 2>&1
 for %%f in (*.vcxproj) do del /Q /F "%%f" >nul 2>&1
@@ -12,10 +11,8 @@ for %%f in (linux_app) do del /Q /F "%%f" >nul 2>&1
 for %%f in (*-autosave.pjd) do del /Q /F "%%f" >nul 2>&1
 for %%f in (*.rc) do del /Q /F "%%f" >nul 2>&1
 
-REM Удаление каталогов, если они существуют
 for %%d in (Debug MinGWUnicodeDebug MinGWUnicodeRelease Release) do (
     if exist "%%d" (
-        echo Удаление каталога %%d...
         rmdir /S /Q "%%d"
     )
 )
